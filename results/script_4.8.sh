1ad36ccd11c984418de4dea472fa82ee7d766a2e
Alexander <ameronk@gmail.com>
2020-05-03 19:37:45 +0300

    All is done

---
+++ results/script_4.8.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+UNAME=$(whoami)
+
+ls -RlA ~ | tee ./481.txt
+echo "============================="
+sed "s/${UNAME}/username/g" ./481.txt | tee ./482a.txt
+echo "============================="
+awk -v ptrn=$UNAME '{gsub(ptrn, "username")}1' ./481.txt | tee ./482b.txt
+echo "============================="
+awk '$0 !~ /total [[:digit:]]+/ {print}' ./481.txt | tee ./483.txt
+echo "============================="
+awk '$9 != "" {print $6" "$7"\t"$9}' ./481.txt | tee ./484.txt
+echo "============================="
+awk '$9 ~ /[0-9]/ {print $9}' ./481.txt | tee ./485.txt
+echo "============================="
+awk '$9 ~ /^.+[.].*$/ {print $9}' ./481.txt | tee ./486.txt
+
